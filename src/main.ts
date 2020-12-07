b41147cc38f6baf7dca265fa34597273cf501086
kodumbeats <brandon.leckemby@gmail.com>
2020-12-07 18:16:21 -0500

    add bulma for css

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "../node_modules/bulma/css/bulma.css";
 
 createApp(App)
   .use(store)
