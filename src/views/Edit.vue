7c02bc5cabcde7cd4a08ce6859ab253792d9009a
kodumbeats <brandon.leckemby@gmail.com>
2020-12-07 18:15:00 -0500

    use vue-easymde as driving editor

---
+++ src/views/Edit.vue
@@ -0,0 +1,17 @@
+<template>
+  <vue-easymde v-model="content" ref="markdownEditor" />
+</template>
+
+<script>
+import VueEasymde from "vue-easymde";
+
+export default {
+  components: {
+    VueEasymde
+  }
+};
+</script>
+
+<style>
+@import "~easymde/dist/easymde.min.css";
+</style>
