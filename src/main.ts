2ca45887fca7ae5b089db48db9d05ac5ec9f112d
kodumbeats <brandon.leckemby@gmail.com>
2020-12-07 19:12:41 -0500

    only import used parts of bulma

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import "../node_modules/bulma/css/bulma.css";
+import "./assets/main.scss";
 
 createApp(App)
   .use(store)
